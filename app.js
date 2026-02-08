aa10b0a4e3bdfc5640eee25dd9652c84279ad7b4
chavish-dev <chaviwow1@gmail.com>
2026-02-08 11:48:03 +0200

    first commit

---
+++ app.js
@@ -0,0 +1,13 @@
+const express = require('express')
+const usersRoutes = require('./routes/user')
+const app = express()
+
+
+app.use('/users', usersRoutes)
+
+mongoose.connection.once('open', () => {
+console.log('Connected to MongoDB')
+})
+
+app.listen(3000, () => console.log(`Server running on port
+${PORT}`))
